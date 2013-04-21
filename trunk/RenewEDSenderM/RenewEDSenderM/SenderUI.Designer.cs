0cf918e11e34490a593ea0f1c08925fac9531a0e
iamtigerli@gmail.com <iamtigerli@gmail.com@b185501b-21b5-8035-b053-276cffa7d249>
2013-04-21 09:21:27 +0000

    First commit

---
+++ trunk/RenewEDSenderM/RenewEDSenderM/SenderUI.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace RenewEDSenderM
+{
+    partial class SenderUI
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btmStart = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btmStart
+            // 
+            this.btmStart.Location = new System.Drawing.Point(296, 59);
+            this.btmStart.Name = "btmStart";
+            this.btmStart.Size = new System.Drawing.Size(75, 23);
+            this.btmStart.TabIndex = 0;
+            this.btmStart.Text = "Start";
+            this.btmStart.UseVisualStyleBackColor = true;
+            this.btmStart.Click += new System.EventHandler(this.btmStart_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(100, 152);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(447, 200);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // SenderUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 410);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btmStart);
+            this.Name = "SenderUI";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btmStart;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ trunk/RenewEDSenderM/RenewEDSenderM/SenderUI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Pipes;
+using System.Diagnostics;
+
+namespace RenewEDSenderM
+{
+    public partial class SenderUI : Form
+    {
+        public SenderUI()
+        {
+            InitializeComponent();
+        }
+
+        private void btmStart_Click(object sender, EventArgs e)
+        {
+            LogManager.Logger.FuncEntryLog(sender, e);
+            //LogManager.Logger.getInstance().logger.Debug(LogManager.Logger.FUN_ENTRY);
+            
+            //LogManager.Logger.WriteInfoLog(LogManager.Logger.FUN_ENTRY);
+            //LogManager.Logger.WriteInfoLog(LogManager.Logger.FUN_EXIT);
+            //LogManager.Logger.getInstance().logger.Debug(LogManager.Logger.FUN_EXIT);
+            LogManager.Logger.FuncExitLog();
+        }
+    }
+}
