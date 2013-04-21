e4dae63e98fa7ac48f01b32a93b4d056ad1c6200
iamtigerli <iamtigerli@gmail.com>
2013-04-21 17:32:57 +0000

    package 校验已通过 CRC16校验已搞明白。 DataLength + 10数字有点多。 DataLength属性调用次数非常多

--- RenewEDSenderM/RenewEDSenderM/Support/DataPackage.cs
+++ RenewEDSenderM/RenewEDSenderM/Support/DataPackage.cs
@@ -11,20 +11,31 @@ namespace RenewEDSenderM.Support
 {
     class Testt
     {
-        void test()
+        /// <summary>
+        /// 测试初始化1
+        /// </summary>
+        public static void testInit()
         {
             // 收到包
             byte[] c = { 0x01, 0x02 };
-            DataPackage dp = new DataPackage { Package = c };
-            dp.Head;
-            dp.DataLength;
+            
+            { 
+            }
+            //Test 初始化
+
+            DataPackage dp = new DataPackage() { Package = c };
+
         }
-        void testsend()
+        /// <summary>
+        /// 测试初始化2
+        /// </summary>
+        public static void testInit2()
         {
-            String s = "<xml>";
-           
-            DataPackage dp = new DataPackage{Head = {0x01}, DataLength=15, Seq = 56, DataBlock=}
-                dp.Package;
+            //发送包
+            int outi;
+            string str = "AD4E729DCD81551ABC7C2983C86D3C6A58A862EAB7762371A0F6A3C83FBDC8EC79D54004F3249D83EE1D8BBE3C906270C99C44D6CF4C7B049B643A7DA312DCC0C5600F18FBB91ADF460802532B9FD2C766EC60477242D78F9CAA0092DA2F1B0492D92951E9EF1E68096B7EA76C41ABF30EB177D316196693D66C3C8AB2C767ED461BABF65A472C57DE9C972B10EDE61E916F38D678C48ED447928E77EF77798D71F71B29AA4A2542C866B4A623FDC6F8838B2D3AD851EC9091C029072A49BB9DDFCCF2831858E339FE73534A65432E5AC65F3E0DF735510BCD64F8B7C2B1F264";
+            DataPackage dp = new DataPackage() { DataLength = 0x000000e4, Seq = 0x1692ec42, CRC=0x6441, DataBlock= DataPackage.GetBytes(str, out outi)};
+            byte [] package = dp.Package;
         }
         
     }
@@ -35,25 +46,73 @@ namespace RenewEDSenderM.Support
     /// </summary>
     class DataPackage
     {
-        private byte[] m_head /*= { 0x68, 0x68, 0x16, 0x16 }*/ ;
+        /// <summary>
+        /// 用于头部校验
+        /// </summary>
+        private static byte[] Verify_Head = { 0x68, 0x68, 0x16, 0x16 };
+        /// <summary>
+        /// 用于尾部校验
+        /// </summary>
+        private static byte[] Verify_Tail = { 0x55, 0xAA, 0x55, 0xAA };
+        /// <summary>
+        /// 数据包长度下限18bytes，用于数据包校验
+        /// </summary>
+        private static uint PkgLengthMin = 18;
+        /// <summary>
+        /// 数据包长度上限，用于数据包校验
+        /// </summary>
+        private static uint PkgLengthMax = uint.MaxValue ;
+        private byte[] m_head = { 0x68, 0x68, 0x16, 0x16 } ;	//缺省值
         private byte[] m_dataLength = new byte[sizeof(uint)];   //m_dataLength = sizeof(m_seq) + sizeof(m_data)
         private byte[] m_seq = new byte[sizeof(uint)];  //
         private byte[] m_data;
         private byte[] m_dummy; //T.B.D. 不考虑？
         private byte[] m_crc16 = new byte[sizeof(UInt16)];
-        private byte[] m_tail = { 0x55, 0xAA, 0x55, 0xAA };
+        private byte[] m_tail = { 0x55, 0xAA, 0x55, 0xAA };		//缺省值
         #region 打包
+        /// <summary>
+        /// 收到的字节流进行解析验证
+        /// 验证失败抛出异常
+        /// </summary>
         public byte[] Package
         {
             //收到一个包之后，映射到各字段
             set
             {
+                //try
+                //{
+                //解析收到的包，验证包长度
+                if (value.Length < PkgLengthMin/*  || value.Length > PkgLengthMax*/)
+                    throw new DataPackageException(DataPackageException.ex_msg7_pkg);
+                //解析出头部
                 Array.Copy(value, m_head, 4);
+                //校验头部
+                
+                //if (!Comparer.Equals(Verify_Head, m_head))
+                    //throw new DataPackageException(DataPackageException.ex_msg1_head);
+                //解析出有效数据长度字段
                 Array.Copy(value, 4, m_dataLength, 0, 4);
+                //解析出序号字段
                 Array.Copy(value, 8, m_seq, 0, 4);
-                Array.Copy(value, 12, m_data, 0, DataLength);
-                Array.Copy(value, 12 + DataLength - 4, m_crc16, 0, 2);
-                Array.Copy(value, 12 + DataLength - 4 + 2, m_tail, 0, 2);
+                //校验有效数据段长度与数据实际长度一致性，PkgLengthMin为除数据段以外的数据大小
+                if((DataLength - 4) != (value.Length - PkgLengthMin))
+                    throw new DataPackageException(DataPackageException.ex_msg6_data);
+                //解析出有效数据段数据,DataLength为AES加密后的指令内容与指令序号的总长度
+                m_data = new byte[DataLength - 4];
+                Array.Copy(value, 12, m_data, 0, DataLength - 4);
+                byte[] data_crc = new byte[DataLength + 10];
+                //解析得到待校验数据[包头到有效数据段]，AES加密后的指令内容：CRC16校验码
+                Array.Copy(value, 0, data_crc, 0, DataLength + 10);
+                //CRC校验
+                if (Support.Encryption.CRC16(data_crc) != 0)
+                    throw new DataPackageException(DataPackageException.ex_msg4_crc);
+                    Array.Copy(value, 12 + DataLength - 4, m_crc16, 0, 2);
+                    Array.Copy(value, 12 + DataLength - 4 + 2, m_tail, 0, 2);
+                //}
+                //catch (ArgumentException aex)
+                //{
+                //    throw 
+                //}
             }
             //发送一个包时，发送整个段
             get
@@ -68,13 +127,6 @@ namespace RenewEDSenderM.Support
                 arraylist.AddRange(m_tail);
                 bytes = arraylist.ToArray(typeof(byte)) as byte[];
                 return bytes;
-                //byte[] p = new byte[m_head.Length + m_dataLength.Length + m_seq.Length + m_data.Length + m_crc16.Length + m_tail.Length];
-                //Array.Copy(m_head, p, m_head.Length);
-                //Array.Copy(m_dataLength, 0, p, m_head.Length, m_dataLength.Length);
-                //Array.Copy(m_seq, 0, p, m_head.Length + m_dataLength.Length, m_seq.Length);
-                //Array.Copy(m_data, 0, p, m_head.Length + m_dataLength.Length + m_seq.Length, m_data.Length);
-                //Array.Copy(m_crc16, 
-                //uint l = m_head.Length + m_dataLength.Length + m
             }
         }
         public byte[] Head
@@ -82,8 +134,8 @@ namespace RenewEDSenderM.Support
             set
             {
                 //T.B.D. 需要校验头部否？
-                byte[] h = { 0x68, 0x68, 0x16, 0x16 };
-                m_head = h;
+                //byte[] h = { 0x68, 0x68, 0x16, 0x16 };
+                m_head = value;
             }
             get
             {
@@ -159,9 +211,55 @@ namespace RenewEDSenderM.Support
                 m_crc16 = BitConverter.GetBytes(value);
             }
         }
+		public byte[] Tail
+        {
+            set
+            {
+                //T.B.D. 需要校验头部否？
+                byte[] h = { 0x68, 0x68, 0x16, 0x16 };
+                m_tail = h;
+            }
+            get
+            {
+                return m_tail;
+            }
+        } 
         #endregion
         #region 解包
         #endregion
+        public static byte[] GetBytes(string hexString, out int discarded)
+        {
+            discarded = 0;
+            string newString = "";
+            char c;
+            // remove all none A-F, 0-9, characters
+            for (int i = 0; i < hexString.Length; i++)
+            {
+                c = hexString[i];
+                if (Uri.IsHexDigit(c))
+                    newString += c;
+                else
+                    discarded++;
+            }
+            // if odd number of characters, discard last character
+            if (newString.Length % 2 != 0)
+            {
+                discarded++;
+                newString = newString.Substring(0, newString.Length - 1);
+            }
+
+            int byteLength = newString.Length / 2;
+            byte[] bytes = new byte[byteLength];
+            string hex;
+            int j = 0;
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hex = new String(new Char[] { newString[j], newString[j + 1] });
+                bytes[i] = Convert.ToByte(hex, 16);
+                j = j + 2;
+            }
+            return bytes;
+        }
         public static void test()
         {
             byte[] m = { 0xe4, 0x00, 0x00, 0x00 };
