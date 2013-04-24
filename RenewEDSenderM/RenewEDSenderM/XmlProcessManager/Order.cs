a7f0073c2a4f764d3ee087774e88d88442965bad
iamtigerli <iamtigerli@gmail.com>
2013-04-24 08:18:58 +0000

    网络连接部分代码集成。

---
+++ RenewEDSenderM/RenewEDSenderM/XmlProcessManager/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RenewEDSenderM.XmlProcessManager
+{
+    class Order
+    {
+        public string sequence = "";
+        public string result="";
+        public string time="";
+        public string heart_result="";
+        public string beginTime="";
+        public string endTime ="";
+        public string period ="";
+        public string keytype ="";
+        public string key ="";
+        public string order="";
+    } 
+}
