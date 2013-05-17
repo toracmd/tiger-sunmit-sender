3e83f74975f201e8082a0c68d5fd3f46b35aeedf
iamtigerli@gmail.com <iamtigerli@gmail.com@b185501b-21b5-8035-b053-276cffa7d249>
2013-05-17 12:33:12 +0000

    删掉RenewEDSenderM工程中的Program.cs SenderUI.cs

---
+++ trunk/RenewEDSenderM20130517/RenewEDSenderM/Support/DataPackageException.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RenewEDSenderM.Support
+{
+    class DataPackageException : ApplicationException
+    {
+        string _message;
+        /// <summary>
+        /// 数据包Head不合法
+        /// </summary>
+        public static readonly string ex_msg1_head = "数据包Head不合法";
+        /// <summary>
+        /// 数据包Tail不合法
+        /// </summary>
+        public static readonly string ex_msg2_tail = "数据包Tail不合法";
+        /// <summary>
+        /// 数据包CRC格式不合法
+        /// </summary>
+        public static readonly string ex_msg3_crc = "数据包CRC格式不合法";
+        /// <summary>
+        /// 数据包CRC数据校验出错
+        /// </summary>
+        public static readonly string ex_msg4_crc = "数据包CRC数据校验出错";
+        /// <summary>
+        /// 数据包序号段不合法
+        /// </summary>
+        public static readonly string ex_msg5_seq = "数据包序号段不合法";
+        /// <summary>
+        /// 数据包数据段出错
+        /// </summary>
+        public static readonly string ex_msg6_data = "数据包数据段出错";
+        /// <summary>
+        /// 数据包长度出错
+        /// </summary>
+        public static readonly string ex_msg7_pkg = "数据包长度出错";
+        //获取父类的错误信息内容
+        public DataPackageException()
+        {
+            _message = base.Message;
+        }
+        public DataPackageException(string str)
+        {
+            _message = str;
+        }
+        //重写message属性
+        public override string Message
+        {
+            get
+            {
+                return _message;
+            }
+        }
+    }
+}
